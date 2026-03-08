7e7d152ba714f53d36fc582b3a5b44403aefe721
Siridhamma-Chamlertwat <s6804062612030@email.kmutnb.ac.th>
2026-03-08 20:04:34 +0700

    Add files via upload

---
+++ lab704.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void printHead(int w);
+void printBody(int w, int h);
+
+int main() {
+
+    int width, height;
+
+    scanf("%d %d", &width, &height);
+
+    printHead(width);
+    printBody(width, height);
+    printHead(width);
+
+    return 0;
+}
+
+void printHead(int w){
+
+    for(int i=0;i<w;i++){
+        printf("* ");
+    }
+    printf("\n");
+}
+
+void printBody(int w, int h){
+
+    for(int i=0;i<h-2;i++){
+
+        printf("*");
+
+        for(int j=0;j<(w-1)*2-1;j++){
+            printf(" ");
+        }
+
+        printf("*\n");
+    }
+}
